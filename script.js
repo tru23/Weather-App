3daf06e7b3380a65dde3e85819fd01f1fa2866af
tru23 <83947699+tru23@users.noreply.github.com>
2024-06-11 18:13:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let button = document.querySelector('.button')
+let inputvalue = document.querySelector('.inputValue')
+let nameVal = document.querySelector('.name');
+let temp = document.querySelector('.temp');
+let desc = document.querySelector('.desc');
+
+
+// ADDING EVENT LISTENER TO SEARCH BUTTON  
+button.addEventListener('click', function(){
+
+    // Fection data from open weather API
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&units=metric&appid=0e435d5fb8378bdaea5722c51114e5b4`)
+    .then(response => response.json())
+    .then(
+        displayData)
+    .catch(err => alert('Wrong City name')); 
+
+})
+
+// Function to diplay weather on html document
+const displayData=(weather)=>{
+    temp.innerText=`${weather.main.temp}°C`
+    
+}
